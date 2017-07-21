92e2d7ed38624f6aa722479205ebf5b63e8051ab
Patrick Lippert <patrick.lippert1@gmail.com>
2017-07-21 11:09:44 -0700

    Added /blogpost to allowed_sites list

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ class User(db.Model):
 
 @app.before_request
 def require_login():
-    allowed_routes = ['login', 'blog', 'index', 'signup']
+    allowed_routes = ['login', 'blog', 'index', 'signup', 'blogpost']
 
     if request.endpoint not in allowed_routes and 'username' not in session:
         return redirect('/login')
